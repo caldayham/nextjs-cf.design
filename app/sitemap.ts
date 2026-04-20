c7bcb0eb44476353bd3001c4ccf81c9edddf1cf6
Cal Day Ham <caldayham@gmail.com>
2026-04-19 19:05:46 -0700

    drop license for now

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -16,6 +16,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://cf.design/case-studies/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://cf.design/inquiries/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
     ...caseStudies,
   ]
 }
